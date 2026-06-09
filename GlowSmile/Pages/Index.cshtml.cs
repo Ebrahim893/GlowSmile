410525f5d2069584c966b127f5228dbcbd4b7569
Ebrahim893 <102203317+Ebrahim893@users.noreply.github.com>
2026-06-10 00:17:47 +0300

    Update Index.cshtml.cs

--- GlowSmile/Pages/Index.cshtml.cs
+++ GlowSmile/Pages/Index.cshtml.cs
@@ -15,14 +15,12 @@ namespace GlowSmile.Pages
         }
 
         public IList<Service> Services { get; set; }
-        // قائمة الدكاترة
         public IList<Doctor> Doctors { get; set; }
 
         public async Task OnGetAsync()
         {
             Services = await _context.Services.ToListAsync();
 
-            // بنجيب الدكاترة وبنقول للسيستم "هات معاك بيانات الخدمة المرتبطة بكل دكتور"
             Doctors = await _context.Doctors
                 .Include(d => d.Service)
                 .ToListAsync();
